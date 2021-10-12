68a1d8b625ce95125dbd3f101b53467f29f6c492
Antoine Langlois <a.lngls@riseup.net>
2021-10-12 16:31:54 +0200

    simple redirections ok

--- sources/parsing/parsing_grammar.c
+++ sources/parsing/parsing_grammar.c
@@ -32,11 +32,19 @@ void io_redirections(t_command *cmd)
 	{
 		if ((*lst)->token == WORD)
 		{
+			if ((*lst)->next->token == PIPE)
+				*lst = (*lst)->next;
 			if ((*lst)->next->token == GREAT)
 			{
 				free(cmd->out_file);
 				cmd->out_file = NULL;
 				cmd->out_file = ms_strdup((*lst)->next->next->arg[0]);
+			}
+			if ((*lst)->next->token == LESS)
+			{
+				free(cmd->in_file);
+				cmd->in_file = NULL;
+				cmd->in_file = ms_strdup((*lst)->next->next->arg[0]);
 			}
 		}
 		*lst = (*lst)->next;
