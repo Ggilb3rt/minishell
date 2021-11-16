8242911729417a994ad9b588b3095c30e5c1282e
Antoine LANGLOIS <alangloi@e3r4p10.42.fr>
2021-11-16 19:24:50 +0100

    yo

--- sources/main.c
+++ sources/main.c
@@ -176,7 +176,8 @@ int	main(int ac, char **av, char **envp)
 		//set_cmd_ready_to_exec(cmd, ms_envp);
 		//print_simple_command(cmd);
 		//print_command(cmd);
-		print_all(cmd);
+		//print_all(cmd);
+		free(cmd);
 		//char	**my_env = convert_envplst_to_tab(ms_envp);
 		//base_pour_exec(cmd, envp, ms_envp);
 		//ms_pipeline(cmd, envp);
